5a036645aa296943688b411a06712a16e92fd189
megumew <megumeww@gmail.com>
2022-09-11 00:38:30 -0400

    Placeholder code trying to get the clipboard copy feature working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use chrono::*;
+use clap::{Parser, Subcommand};
+extern crate clipboard;
+
+use clipboard::ClipboardContext;
+use clipboard::ClipboardProvider;
+
+/// A Discord Timestamp Generator
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Test {
+        /// lists test values
+        #[clap(short, long, action)]
+        list: bool,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let output = format!("<t:{}>", Utc::now().timestamp());
+    println!("{}", &output);
+    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+    ctx.set_contents(output.to_owned()).unwrap();
+    println!("Copied to clipboard!");
 }
